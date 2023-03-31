5ee1d4dbdcdf96c0b59ea9553fb83243c8c77612
2022Hop2c-Orgil <2022hop2c-orgil@iMac-18s-iMac.local>
2023-03-31 19:56:10 +0800

    header done

--- Frontend/src/components/header.jsx
+++ Frontend/src/components/header.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import "../css/header.css";
 import headerImg1 from "../assets/header1.svg";
 import hamburger from "../assets/hamburger.svg";
+import logo from "../assets/Logo.svg";
 export const Header = () => {
   return (
     <div className="headerBig">
       <div className="header">
-        <div className="headerLogo"></div>
+        <div className="headerContent1">
+          <img className="headerLogo" src={logo} />
+          <div className="headerhaichu">Хайчихуу?</div>
+        </div>
         <div className="headerContent">
           <button className="headerHostBtn">Become A Host</button>
           <button className="headerContentRight">
