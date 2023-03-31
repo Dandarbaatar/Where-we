35ada95803a7e20597d4cafeff8aed7d28873221
2022hop2c-chingun <2022hop2c-chingun@iMac-von-iMac-14.local>
2023-03-31 19:26:10 +0800

    notdone

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,17 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-import './App.css';
+/** @format */
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
 import { Home } from "./pages/Home";
+import { Search } from "./pages/Search";
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
     </BrowserRouter>
   );
 }
---
+++ Frontend/src/components/filter.js
@@ -0,0 +1,32 @@
+/** @format */
+import { Button } from "react-bootstrap";
+import Ex from "../assets/Vector (1).svg";
+
+export const Filter = ({ name }) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        backgroundColor: "#E0E2E6",
+        flexDirection: "row",
+        alignItems: "center",
+        borderRadius: "5vw",
+        paddingLeft: "1vw",
+      }}
+    >
+      <div>{name}</div>
+      <Button
+        style={{
+          backgroundColor: "transparent",
+          border: "none",
+          display: "flex",
+          alignItems: "center",
+          width: "2.5vw",
+          height: "100%",
+        }}
+      >
+        <img src={Ex}></img>
+      </Button>
+    </div>
+  );
+};
