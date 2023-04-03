e3cc62746add6ecb1dec6b555719da5599016414
2022Hop2c-Orgil <2022hop2c-orgil@iMac-18s-iMac.local>
2023-04-03 18:10:58 +0800

    l

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,9 +1,20 @@
 import { Header } from "../components/header";
+import "../css/home.css";
 export const Home = () => {
   return (
     <div className="homeBig">
       <Header />
-      <div></div>
+      <div className="homePage1">
+        <div className="homePage1Content">
+          <div className="homePage1Topic">FIND</div>
+          <div className="homePage1Contents">
+            <div className="homePage1Content">Rooms</div>
+            <div className="homePage1Content">Flats</div>
+            <div className="homePage1Content">Hostels</div>
+            <div className="homePage1Content">villas</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
