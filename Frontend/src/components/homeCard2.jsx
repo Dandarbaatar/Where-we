aa5a901456a898f7ebfdd09e81c2189723881bbd
2022Hop2c-Orgil <2022hop2c-orgil@iMac-18s-iMac.local>
2023-04-14 18:47:49 +0800

    ddsa

--- Frontend/src/components/homeCard2.jsx
+++ Frontend/src/components/homeCard2.jsx
@@ -1,5 +1,9 @@
 import "../App.css";
 import zurh from "../assets/homeCardLike.svg";
+import savar from "../assets/pet.svg";
+import usniiOngots from "../assets/usniiOngots.svg";
+import car from "../assets/car.svg";
+import or from "../assets/bed.svg";
 export const HomeCard2 = () => {
   return (
     <div className="homeCard2">
@@ -7,9 +11,18 @@ export const HomeCard2 = () => {
         <button>
           <img src={zurh} />
         </button>
-        <div></div>
+        <div>$ 1000 - 5000 USD</div>
       </div>
-      <div className="homeCard2Under"></div>
+      <div className="homeCard2Under">
+        <div className="homeCard2UnderText1">Well Furnished Apartment</div>
+        <div className="homeCard2UnderText2">100 Smart Street, LA, USA</div>
+        <div className="homeCard2UnderImgs">
+          <img className="homeCard2UnderImg" src={or} />
+          <img className="homeCard2UnderImg" src={usniiOngots} />
+          <img className="homeCard2UnderImg" src={car} />
+          <img className="homeCard2UnderImg" src={savar} />
+        </div>
+      </div>
     </div>
   );
 };
