1b0b446e93d64098229e94c1c6ee647137d47382
2022Hop2c-Orgil <2022hop2c-orgil@iMac-18s-iMac.local>
2023-05-17 16:01:56 +0800

    s

--- Frontend/src/components/dark-light-Btn.jsx
+++ Frontend/src/components/dark-light-Btn.jsx
@@ -2,13 +2,13 @@ import React, { useState } from "react";
 import { createContext } from "react";
 export const ThemeContext = createContext(null);
 export const Darklighbttn = () => {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState("dark");
   const handleToggle = () => {
     setTheme(!theme);
   };
   return (
     <ThemeContext.Provider>
-      <button onClick={handleToggle} className={theme ? "dark" : "light"}>
+      <button onClick={handleToggle} className={theme ? "light" : "dark"}>
         <div id="Btn"></div>
         <div id="Btn1"></div>
       </button>
