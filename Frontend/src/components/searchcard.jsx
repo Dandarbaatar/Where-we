8da52445e6f9422379686531732161fa729f7d9b
Ermuun Khanburged <dandarbaatar@Ermuuns-MacBook-Pro.local>
2023-05-28 11:26:57 +0800

    uhyee

--- Frontend/src/components/searchcard.jsx
+++ Frontend/src/components/searchcard.jsx
@@ -30,273 +30,215 @@ export const SearchCard = ({
         marginRight: "9vw",
       }}
     >
-      <Link to="/detail" style={{ textDecoration: "none", color: "#FFFFFF" }}>
+      <div
+        style={{
+          width: "100%",
+          height: "26vw",
+          backgroundColor: "#E0E2E6",
+          borderTopLeftRadius: "1vw",
+          borderTopRightRadius: "1vw",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+          backgroundColor: "#80858E",
+        }}
+      >
+        <img
+          style={{
+            display: "flex",
+            zIndex: "0",
+            position: "absolute",
+            width: "36.5vw",
+            height: "26vw",
+            borderTopRightRadius: "1vw",
+            borderTopLeftRadius: "1vw",
+          }}
+          src={image}
+        ></img>
+        <div
+          style={{
+            width: "95%",
+            display: "flex",
+            justifyContent: "flex-end",
+          }}
+        >
+          <button
+            style={{
+              border: "none",
+              backgroundColor: "transparent",
+              marginTop: "1.7vw",
+            }}
+          >
+            <img alt="" style={{ width: "2.5vw" }}></img>
+          </button>
+        </div>
         <div
           style={{
             width: "100%",
-            height: "26vw",
-            backgroundColor: "#E0E2E6",
-            borderTopLeftRadius: "1vw",
-            borderTopRightRadius: "1vw",
             display: "flex",
-            flexDirection: "column",
+            flexDirection: "row",
             justifyContent: "space-between",
+            marginBottom: "1.7vw",
+
+            zIndex: "1",
           }}
         >
           <div
             style={{
-              width: "95%",
               display: "flex",
-              justifyContent: "flex-end",
-            }}
-          >
-            <button
-              style={{
-                border: "none",
-                backgroundColor: "transparent",
-                marginTop: "1.7vw",
-              }}
-            >
-              <img alt="" style={{ width: "2.5vw" }}></img>
-            </button>
-          </div>
-          <div
-            style={{
-              width: "100%",
-              height: "26vw",
-              backgroundColor: "#E0E2E6",
-              borderTopLeftRadius: "1vw",
-              borderTopRightRadius: "1vw",
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "space-between",
-              backgroundColor: "#80858E",
+              flexDirection: "row",
+              justifyContent: "space-evenly",
+              width: "56%",
             }}
           >
             <img
+              alt=""
               style={{
-                display: "flex",
-                zIndex: "0",
-                position: "absolute",
-                width: "36.5vw",
-                height: "26vw",
-                borderTopRightRadius: "1vw",
-                borderTopLeftRadius: "1vw",
+                width: "5.5vw",
+                height: "5.5vw",
+                borderRadius: "10vw",
+                backgroundColor: "white",
               }}
-              src={image}
             ></img>
             <div
               style={{
-                width: "95%",
                 display: "flex",
-                justifyContent: "flex-end",
+                flexDirection: "column",
               }}
             >
-              <button
-                style={{
-                  border: "none",
-                  backgroundColor: "transparent",
-                  marginTop: "1.7vw",
-                }}
-              >
-                <img alt="" style={{ width: "2.5vw" }}></img>
-              </button>
-            </div>
-            <div
-              style={{
-                width: "100%",
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "space-between",
-                marginBottom: "1.7vw",
-
-                zIndex: "1",
-              }}
-            >
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "row",
-                  justifyContent: "space-evenly",
-                  width: "56%",
-                }}
-              >
-                <img
-                  alt=""
-                  style={{
-                    width: "5.5vw",
-                    height: "5.5vw",
-                    borderRadius: "10vw",
-                    backgroundColor: "white",
-                  }}
-                ></img>
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "column",
-                  }}
-                >
-                  <div style={{ fontSize: "1vw", color: "#FFFFFF" }}>
-                    Listed by:
-                  </div>
-                  <div
-                    style={{
-                      fontSize: "1.3vw",
-                      fontWeight: "700",
-                      color: "#FFFFFF",
-                    }}
-                  >
-                    {name}
-                  </div>
-                  <div style={{ fontSize: "1.1vw", color: "#FFFFFF" }}>
-                    For:{price}
-                  </div>
-                </div>
-                <div style={{ fontSize: "1vw" }}>Listed by:</div>
-                <div style={{ fontSize: "1.3vw", fontWeight: "700" }}>
-                  {name}
-                </div>
-                <div style={{ fontSize: "1.1vw" }}>For:{price}$</div>
+              <div style={{ fontSize: "1vw", color: "#FFFFFF" }}>
+                Listed by:
               </div>
-              <button
-                style={{
-                  height: "1vw",
-                  marginTop: "2.5vw",
-                  marginRight: "2vw",
-                  border: "none",
-                  backgroundColor: "transparent",
-                }}
-              >
-                <img
-                  style={{ width: "4vw" }}
-                  alt=""
-                  src="slider indicators.svg"
-                ></img>
-              </button>
-            </div>
-          </div>
-          <div
-            style={{
-              display: "flex",
-              width: "100%",
-              height: "11vw",
-              backgroundColor: "#565C68",
-              borderBottomLeftRadius: "1vw",
-              borderBottomRightRadius: "1vw",
-              paddingLeft: "1.5vw",
-              flexDirection: "column",
-              justifyContent: "space-evenly",
-            }}
-          >
-            <div>
               <div
                 style={{
-                  fontSize: "1.2vw",
+                  fontSize: "1.3vw",
                   fontWeight: "700",
                   color: "#FFFFFF",
                 }}
               >
-                Temp
+                {name}
               </div>
-              <div
-                style={{
-                  fontSize: "0.8vw",
-                  color: "#DDDDDD",
-                  height: "2.1vw",
-                  overflow: "hidden",
-                }}
-              >
-                Description: {description}
+              <div style={{ fontSize: "1.1vw", color: "#FFFFFF" }}>
+                For:{price}
               </div>
-              <div
-                style={{ display: "flex", flexDirection: "row", gap: "0.4vw" }}
-              >
-                <img style={{ width: "1.3vw" }} alt="" src="bed.svg"></img>
-                <div style={{ fontSize: "1vw" }}>{bedrooms}</div>
+            </div>
+            <div style={{ fontSize: "1vw" }}>Listed by:</div>
+          </div>
+          <button
+            style={{
+              height: "1vw",
+              marginTop: "2.5vw",
+              marginRight: "2vw",
+              border: "none",
+              backgroundColor: "transparent",
+            }}
+          >
+            <img
+              style={{ width: "4vw" }}
+              alt=""
+              src="slider indicators.svg"
+            ></img>
+          </button>
+        </div>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          width: "100%",
+          height: "11vw",
+          backgroundColor: "#565C68",
+          borderBottomLeftRadius: "1vw",
+          borderBottomRightRadius: "1vw",
+          paddingLeft: "1.5vw",
+          flexDirection: "column",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <div>
+          <div
+            style={{
+              fontSize: "1.2vw",
+              fontWeight: "700",
+              color: "#FFFFFF",
+            }}
+          >
+            {placetype}
+          </div>
+          <div
+            style={{
+              fontSize: "0.8vw",
+              color: "#DDDDDD",
+              height: "2.1vw",
+              overflow: "hidden",
+            }}
+          >
+            Description: {description}
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              width: "27%",
+            }}
+          >
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                gap: "0.4vw",
+              }}
+            >
+              <img style={{ width: "1.3vw" }} alt="" src="bed.svg"></img>
+              <div style={{ fontSize: "1vw", color: "#FFFFFF" }}>
+                {bedrooms}
               </div>
-              <div
-                style={{ display: "flex", flexDirection: "row", gap: "0.4vw" }}
-              >
-                <img style={{ width: "1.1vw" }} alt="" src="bath.svg"></img>
-                <div style={{ fontSize: "1vw" }}>{bathrooms}</div>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                gap: "0.4vw",
+              }}
+            >
+              <img style={{ width: "1.1vw" }} alt="" src="bath.svg"></img>
+              <div style={{ fontSize: "1vw", color: "#FFFFFF" }}>
+                {bathrooms}
               </div>
-              <div
-                style={{ display: "flex", flexDirection: "row", gap: "0.4vw" }}
-              >
-                <img style={{ width: "1.5vw" }} alt="" src="car.svg"></img>
-                <div style={{ fontSize: "1vw" }}>{parkings}</div>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                gap: "0.4vw",
+              }}
+            >
+              <img style={{ width: "1.5vw" }} alt="" src="car.svg"></img>
+              <div style={{ fontSize: "1vw", color: "#FFFFFF" }}>
+                {parkings}
               </div>
-              <div
-                style={{ display: "flex", flexDirection: "row", gap: "0.4vw" }}
-              >
-                <img style={{ width: "1.2vw" }} alt="" src="pet.svg"></img>
-                <div style={{ fontSize: "1vw" }}></div>
-              </div>
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "row",
-                  justifyContent: "space-between",
-                  width: "27%",
-                }}
-              >
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "row",
-                    gap: "0.4vw",
-                  }}
-                >
-                  <img style={{ width: "1.3vw" }} alt="" src="bed.svg"></img>
-                  <div style={{ fontSize: "1vw", color: "#FFFFFF" }}>
-                    {bedrooms}
-                  </div>
-                </div>
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "row",
-                    gap: "0.4vw",
-                  }}
-                >
-                  <img style={{ width: "1.1vw" }} alt="" src="bath.svg"></img>
-                  <div style={{ fontSize: "1vw", color: "#FFFFFF" }}>
-                    {bathrooms}
-                  </div>
-                </div>
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "row",
-                    gap: "0.4vw",
-                  }}
-                >
-                  <img style={{ width: "1.5vw" }} alt="" src="car.svg"></img>
-                  <div style={{ fontSize: "1vw", color: "#FFFFFF" }}>
-                    {parkings}
-                  </div>
-                </div>
-              </div>
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "row",
-                  justifyContent: "space-between",
-                  alignItems: "center",
-                  width: "60%",
-                  height: "3vw",
-                }}
-              >
-                <div
-                  style={{
-                    color: "#DDDDDD",
-                    fontSize: "1vw",
-                    fontWeight: "600",
-                  }}
-                >
-                  {placetype} on Rent
-                </div>
-                {/* <hr
+            </div>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              width: "60%",
+              height: "3vw",
+            }}
+          >
+            <div
+              style={{
+                color: "#DDDDDD",
+                fontSize: "1vw",
+                fontWeight: "600",
+              }}
+            >
+              {placetype} on Rent
+            </div>
+            {/* <hr
             style={{ color: "#9A9A9A", fontSize: "0.8vw", fontWeight: "600" }}
           ></div>
           <hr
@@ -307,18 +249,16 @@ export const SearchCard = ({
               border: "none",
             }}
           /> */}
-                <div
-                  style={{
-                    color: "#9A9A9A",
-                    fontSize: "0.8vw",
-                    fontWeight: "600",
-                  }}
-                ></div>
-              </div>
-            </div>
+            <div
+              style={{
+                color: "#9A9A9A",
+                fontSize: "0.8vw",
+                fontWeight: "600",
+              }}
+            ></div>
           </div>
         </div>
-      </Link>
+      </div>
     </div>
   );
 };
